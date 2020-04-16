6310d6ee7771bb1cfbf68627e729f417966292fe
Joe Small <small.joseph@outlook.com>
2020-04-16 03:08:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+  badges=[]
+  people.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
+  end
+  return badges
+end
+
+def assign_rooms(number)
+  return "Hello, #{name}! You'll be assigned to room #{number}"
+  
+end 
